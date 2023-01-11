c9d360f095bedab771b48490ed93200f52ed4011
moses <leecmoses@gmail.com>
2023-01-11 09:01:02 -0500

    Create category item component with styles

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
+import CategoryItem from "./components/category-item/category-item.component";
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Hats",
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "Jackets",
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "Sneakers",
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "Womens",
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "Mens",
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ id, title }) => (
-        <div className="category-container" key={id}>
-          {/* <img></img> */}
-          <div className="category-body-container">
-            <h1>{title}</h1>
-            <p>Show Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
