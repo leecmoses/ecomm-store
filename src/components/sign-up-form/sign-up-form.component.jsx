92b7fe3b0dcea4ab64e915cb17f53c859a082ef8
moses <leecmoses@gmail.com>
2023-01-20 14:19:03 -0500

    Create SignUpForm component and a generalized handleChange funct for the form

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+// Use object for the generalized handleChange
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // Spread in the object and modify one value on the object
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+
+        <label>Email</label>
+        <input
+          email="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
